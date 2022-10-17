bc6b7180d76fd5bb62c259d80ddb1de7c9092720
ERSEN GULTEPE <egultepe@sgk.gov.tr>
2022-10-18 00:07:23 +0300

    db connection changed

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '94.73.150.29'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'phy'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'u7629406_phyuser'),
+            'username' => env('DB_USERNAME', 'u7629406_phydb'),
+            'password' => env('DB_PASSWORD', 'UV_H5Jyq_o37:i@1'),
             'unix_socket' =>  '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
